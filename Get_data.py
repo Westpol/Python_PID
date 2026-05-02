eb7184dad19b02646db7b6f7fc16ce3eda9d0587
Benno <81779508+Westpol@users.noreply.github.com>
2026-05-02 14:51:59 +0200

    Time based to iteration based speed

--- Get_data.py
+++ Get_data.py
@@ -6,6 +6,11 @@ pygame.init()   # initialize Window
 screen = pygame.display.set_mode((1000, 500))
 pygame.display.set_caption("PID Controller")
 width, height = pygame.display.get_window_size()
+clock = pygame.time.Clock()
+
+sim_speed:int = 10
+sim_step:int = 0
+dt:float = 1/sim_speed
 
 MouseSetpoint:float = width/2
 x:float = width/2+250     # position on the x-axis
@@ -13,25 +18,32 @@ vx:float = 0.0    # velocity on the x-axis
 ax:float = 0.0    # acceleration on the x-axis
 i_value:float = 0  # I term thing
 
-p:float = 200     # p term multiplier
+master_scaling:float = 10 # scales every term the same way
+p:float = 5    # p term multiplier
 i:float = 1   # i term multiplier
-d:float = 50  # d term multiplier
+d:float = 24   # d term multiplier
 ff = 50 # Feed Forward (boosts object with setpoint derivative)
 
+p /= master_scaling
+i /= master_scaling
+d /= master_scaling
+ff /= master_scaling
+
 wind:bool = False  # If wind is being simulated (explaining I term)
-i_enable:bool = True
+i_enable:bool = False
+correct_d:bool = False
 
 last_error:float = 0.0
 
 FF_enable = True # if Feed Forward is being enabled
 
-ms = []     # data
-acc = []
-vel = []
-error = []
-i_vals = []
-baseline = []
-actualVals = []
+collect_ms = []     # data
+collect_acc = []
+collect_vel = []
+collect_error = []
+collect_i_vals = []
+collect_baseline = []
+collect_actualVals = []
 
 
 def drawRect(pos, sp):
@@ -41,42 +53,42 @@ def drawRect(pos, sp):
 
 
 def movementUpdate(pos, velocity, acceleration):
-    global lasttime
-    deltaT = time.time() - lasttime
-    lasttime = time.time()
-
-    velocity += acceleration * deltaT
+    global dt
+    velocity += acceleration * dt
+    velocity *= 0.98 ** dt    # add a bit of friction
     if wind:
-        velocity -= 1
-    pos += velocity * deltaT
+        velocity -= 1 * dt
+    pos += velocity * dt
 
     return pos, velocity
 
 
 def PID(setpoint, position, velocity):
-    global ax, p, i, d, i_value
-    acceleration = (setpoint - position) * (p / 10)
-    acceleration += (- velocity) * (d / 10)
+    global ax, p, i, d, i_value, last_error, correct_d, dt
+    error:float = setpoint - position
+    acceleration = error * p # p
+    if correct_d:
+        acceleration += (error - last_error) / dt * d # d
+        last_error = error
+    else:
+        acceleration += -velocity * d
     if i_enable:
-        i_value += position-setpoint
-        acceleration -= i_value * (i/100)
+        i_value += error * dt
+        acceleration += i_value * i   # i
 
     return acceleration
 
 
 def collect_data():
-    global ms, acc, vel, error, i_value
-    ms.append((time.time()-starttime)*1000)
-    acc.append(ax)
-    vel.append(vx)
-    i_vals.append(i_value / 100.0)
-    error.append((x - MouseSetpoint)*3)
-    baseline.append((width - MouseSetpoint) * 2)
-    actualVals.append((width - x) * 2)
-
-
-starttime = time.time()
-lasttime = time.time()
+    global collect_ms, collect_acc, collect_vel, collect_error, i_value, sim_step
+    collect_ms.append(sim_step)
+    sim_step += 1
+    collect_acc.append(ax)
+    collect_vel.append(vx)
+    collect_i_vals.append(i_value / 100.0)
+    collect_error.append((x - MouseSetpoint) * 3)
+    collect_baseline.append((width - MouseSetpoint) * 2)
+    collect_actualVals.append((width - x) * 2)
 
 # wait until mouse button is pressed
 while 1:
@@ -91,10 +103,12 @@ while 1:
     pygame.display.flip()
     screen.fill((0, 0, 0))
 
+starttime = time.time()
 # for z in range(500):   # for linux
-for z in range(10000):    # for windows
+for z in range(120):    # for windows
 
 # UI
+    clock.tick(60)
     pygame.event.pump()     # get new mouse info
 
     MouseSetpoint = pygame.mouse.get_pos()[0]   # save mouse info to int
@@ -105,13 +119,11 @@ for z in range(10000):    # for windows
 
 # Physics
 
-    x, vx = movementUpdate(x, vx, ax)   # physics
-    ax = PID(MouseSetpoint, x, vx)
+    for f in range(sim_speed):
+        x, vx = movementUpdate(x, vx, ax)   # physics
+        ax = PID(MouseSetpoint, x, vx)
 
-    if ax > 3000:   # limit max acceleration
-        ax = 3000
-    elif ax < -3000:
-        ax = -3000
+        ax = max(min(ax, 50 * dt), -50 * dt)
 
 # Data collection
 
@@ -120,12 +132,12 @@ for z in range(10000):    # for windows
 
 pygame.quit()
 
-#plt.plot(ms, acc, label="Acceleration")
-#plt.plot(ms, vel, label="Velocity")
-#plt.plot(ms, error, label="Error")
-plt.plot(ms, i_vals, label="I-term")
-plt.plot(ms, baseline, label="Setpoint")
-plt.plot(ms, actualVals, label="Position")
+#plt.plot(collect_ms, collect_acc, label="Acceleration")
+#plt.plot(collect_ms, collect_vel, label="Velocity")
+#plt.plot(collect_ms, collect_error, label="Error")
+#plt.plot(collect_ms, collect_i_vals, label="I-term")
+plt.plot(collect_ms, collect_baseline, label="Setpoint")
+plt.plot(collect_ms, collect_actualVals, label="Position")
 
 plt.legend()
 plt.show()
